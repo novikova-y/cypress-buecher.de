240b7d91b35a374ed926a336bb11dece4263ae85
Yulia Novikova <ysn.novikova@gmail.com>
2025-04-26 22:10:17 +0200

    Refactor: moved cookies setup to custom command 'setConsentCookies'

--- cypress/e2e/main_page.cy.js
+++ cypress/e2e/main_page.cy.js
@@ -3,35 +3,7 @@ import { mainPage } from "../support/pages/mainPage";
 describe('Buecher.de main page', () => {
   beforeEach(() => {
     // Set cookies for consent acceptance
-    cy.setCookie('klaro', JSON.stringify({
-      "buecher_session": true,
-      "google_services_session": false,
-      "klaro": true,
-      "buecher_statistics": false,
-      "google_services_statistics": false,
-      "buecher_personalization": false,
-      "buecher_advertising": false,
-      "adtraction": false,
-      "awin": false,
-      "billiger": false,
-      "bing": false,
-      "connexity": false,
-      "taboola": false,
-      "criteo": false,
-      "facebook": false,
-      "getback": false,
-      "google_ads": false,
-      "google_analytics": false,
-      "gumgum": false,
-      "outbrain": false,
-      "rtb_house": false,
-      "tradedoubler": false,
-      "xandr": false,
-      "necessary": false,
-      "functional": false,
-      "personalization": false,
-      "marketing": false
-    }));
+    cy.setConsentCookies();
     mainPage.visit();
   });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,36 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-iframe';
+
+// Set cookies for consent acceptance
+Cypress.Commands.add('setConsentCookies', () => {
+    cy.setCookie('klaro', JSON.stringify({
+      "buecher_session": true,
+      "google_services_session": false,
+      "klaro": true,
+      "buecher_statistics": false,
+      "google_services_statistics": false,
+      "buecher_personalization": false,
+      "buecher_advertising": false,
+      "adtraction": false,
+      "awin": false,
+      "billiger": false,
+      "bing": false,
+      "connexity": false,
+      "taboola": false,
+      "criteo": false,
+      "facebook": false,
+      "getback": false,
+      "google_ads": false,
+      "google_analytics": false,
+      "gumgum": false,
+      "outbrain": false,
+      "rtb_house": false,
+      "tradedoubler": false,
+      "xandr": false,
+      "necessary": false,
+      "functional": false,
+      "personalization": false,
+      "marketing": false
+    }));
+  });
