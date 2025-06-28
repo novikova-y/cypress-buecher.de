57ec8a0c463e9929f812975102d77b3fde101ae0
Yulia Novikova <ysn.novikova@gmail.com>
2025-06-28 23:33:56 +0200

    Remove obsolete tests after redesign, fix broken ones, and temporarily comment out others

--- cypress/e2e/mainPage.cy.js
+++ cypress/e2e/mainPage.cy.js
@@ -36,96 +36,48 @@ describe('Buecher.de main page', () => {
       mainPage.getShoppingBasket().should('be.visible');
     });
 
-    it('should display the Banner Slider', () => {
-      mainPage.getBannerSlider().should('be.visible');
+    it('should display the Newsletter registration button', () => {
+      mainPage.getNewsletterButton().should('be.visible');
     });
 
-    it('should display sections', () => {
-      mainPage.getBuchSection().should('be.visible');
-      mainPage.getEBookSection().should('be.visible');
-      mainPage.getTolinoSection().should('be.visible');
-      mainPage.getHörbuchSection().should('be.visible');
-      mainPage.getKinderbuchSection().should('be.visible');
-      mainPage.getJugendbuchSection().should('be.visible');
-      mainPage.getRatgeberSection().should('be.visible');
-      mainPage.getSchuleSection().should('be.visible');
-      mainPage.getKalenderSection().should('be.visible');
-      mainPage.getEnglischeBuecherSection().should('be.visible');
-      mainPage.getSaleSection().should('be.visible');
-      mainPage.getSpielzeugSection().should('be.visible');
-      mainPage.getMusikSection().should('be.visible');
-      mainPage.getFilmSection().should('be.visible');
-      mainPage.getGamesSection().should('be.visible');
-      mainPage.getFanartikelSection().should('be.visible');
-      mainPage.getTechnikSection().should('be.visible');
-      mainPage.getWohnenSection().should('be.visible');
-      mainPage.getSchreibwarenSection().should('be.visible');
-      mainPage.getBuchreihenSection().should('be.visible');
-      mainPage.getMagazinSection().should('be.visible');
+    it('should display the Newsletter registration email input', () => {
+      mainPage.getNewsletterButton().should('be.visible');
     });
+
   });
 
-  describe('Sidebar elements', () => {
-    it('should display Sidebar sections', () => {
-      mainPage.getSidebarBestsellerSection().should('be.visible');
-      mainPage.getSidebarNeuheitenSection().should('be.visible');
-      mainPage.getSidebarVorschauSection().should('be.visible');
-      mainPage.getSidebarPreishitsSection().should('be.visible');
-      mainPage.getSidebarGeschenkgutscheineSection().should('be.visible');
-      mainPage.getSidebarBuchreihenSection().should('be.visible');
-      mainPage.getSidebarKochbuchshopSection().should('be.visible');
-      mainPage.getSidebarGregShopSection().should('be.visible');
-      mainPage.getSidebarReisefuehrerSection().should('be.visible');
-      mainPage.getSidebarStarWarsShopSection().should('be.visible');
-      mainPage.getSidebarNintendoShopSection().should('be.visible');
-      mainPage.getSidebarStabiloShopSection().should('be.visible');
-      mainPage.getSidebarBravoHitsSection().should('be.visible');
-      mainPage.getSidebarLegoshopSection().should('be.visible');
-      mainPage.getSidebarGUShopSection().should('be.visible');
-    });
+  // describe('Footer elements', () => {
+  //   it('should display Footer links', () => {
+  //     mainPage.getFooterAccountLink().should('be.visible');
+  //     mainPage.getFooterGutscheinLink().should('be.visible');
+  //     mainPage.getFooterLieferungLink().should('be.visible');
+  //     mainPage.getFooterRuecksendungLink().should('be.visible');
+  //     mainPage.getFooterWiderrufLink().should('be.visible');
+  //     mainPage.getFooterRechnungLink().should('be.visible');
+  //     mainPage.getFooterPaypalLink().should('be.visible');
+  //     mainPage.getFooterKreditkarteLink().should('be.visible');
+  //     mainPage.getFooterKontaktLink().should('be.visible');
+  //     mainPage.getFooterGeschenkgutscheineLink().should('be.visible');
+  //     mainPage.getFooterDatenschutzLink().should('be.visible');
+  //     mainPage.getFooterFaqLink().should('be.visible');
+  //     mainPage.getFooterAgbLink().should('be.visible');
+  //     mainPage.getFooterUeberUnsLink().should('be.visible');
+  //     mainPage.getFooterImpressumLink().should('be.visible');
+  //     mainPage.getFooterPartnerprogrammLink().should('be.visible');
+  //     mainPage.getFooterPressemitteilungenLink().should('be.visible');
+  //   });
+  // });
 
-    it('should display Bestseller blocks', () => {
-      mainPage.getBestsellerBooksBlock().should('be.visible');
-      mainPage.getBestsellerMoviesBlock().should('be.visible');
-      mainPage.getBestsellerMusicBlock().should('be.visible');
-      mainPage.getBestsellerToysBlock().should('be.visible');
-      mainPage.getBestsellerGamesBlock().should('be.visible');
-      mainPage.getBestsellerStationeryBlock().should('be.visible');
-    });
-  });
-
-  describe('Footer elements', () => {
-    it('should display Footer links', () => {
-      mainPage.getFooterAccountLink().should('be.visible');
-      mainPage.getFooterGutscheinLink().should('be.visible');
-      mainPage.getFooterLieferungLink().should('be.visible');
-      mainPage.getFooterRuecksendungLink().should('be.visible');
-      mainPage.getFooterWiderrufLink().should('be.visible');
-      mainPage.getFooterRechnungLink().should('be.visible');
-      mainPage.getFooterPaypalLink().should('be.visible');
-      mainPage.getFooterKreditkarteLink().should('be.visible');
-      mainPage.getFooterKontaktLink().should('be.visible');
-      mainPage.getFooterGeschenkgutscheineLink().should('be.visible');
-      mainPage.getFooterDatenschutzLink().should('be.visible');
-      mainPage.getFooterFaqLink().should('be.visible');
-      mainPage.getFooterAgbLink().should('be.visible');
-      mainPage.getFooterUeberUnsLink().should('be.visible');
-      mainPage.getFooterImpressumLink().should('be.visible');
-      mainPage.getFooterPartnerprogrammLink().should('be.visible');
-      mainPage.getFooterPressemitteilungenLink().should('be.visible');
-    });
-  });
-
-  describe('Social media and service icons', () => {
-    it('should display social media and service icons', () => {
-      mainPage.getFacebookIcon().should('be.visible');
-      mainPage.getInstagramIcon().should('be.visible');
-      mainPage.getYouTubeIcon().should('be.visible');
-      mainPage.getRechnungIcon().should('be.visible');
-      mainPage.getPayPalIcon().should('be.visible');
-      mainPage.getKreditkarteIcon().should('be.visible');
-      mainPage.getPaybackServiceIcon().should('be.visible');
-      mainPage.getDeutschePostDHLIcon().should('be.visible');
-    });
-  });
+  // describe('Social media and service icons', () => {
+  //   it('should display social media and service icons', () => {
+  //     mainPage.getFacebookIcon().should('be.visible');
+  //     mainPage.getInstagramIcon().should('be.visible');
+  //     mainPage.getYouTubeIcon().should('be.visible');
+  //     mainPage.getRechnungIcon().should('be.visible');
+  //     mainPage.getPayPalIcon().should('be.visible');
+  //     mainPage.getKreditkarteIcon().should('be.visible');
+  //     mainPage.getPaybackServiceIcon().should('be.visible');
+  //     mainPage.getDeutschePostDHLIcon().should('be.visible');
+  //   });
+  // });
 });
--- cypress/support/pages/mainPage.js
+++ cypress/support/pages/mainPage.js
@@ -14,173 +14,24 @@ export class MainPage {
         return cy.get('#qusearchfield');
     }
     getRegistrationLink() {
-        return cy.get('a[data-gat="header;genericNavi;Anmelden"]');
+        return cy.get('a[alt="Mein Kundenkonto"]');
     }
     getWishlistIcon() {
-        return cy.get('a[data-gat="header;genericNavi;Merkliste"]');
+        return cy.get('a[data-tooltip="Meine Merkliste"]');
     }
     getPaybackIcon() {
-        return cy.get('img.logo-payback');
+    return cy.get('img[alt^="PAYBACK Logo"]');
     }
     getShoppingBasket() {
-        return cy.get('a[data-gat="header;Warenkorb"]:visible').first();
-      }
-    getBuchSection() {
-        return cy.get('a[data-gat="primaryNavi;Buch"]');
+    return cy.get('a[data-behavior="cartCount"]').contains('Warenkorb');
     }
-    getEBookSection() {
-        return cy.get('a[data-gat="primaryNavi;eBook"]');
-    }
-    getTolinoSection() {
-        return cy.get('a[data-gat="primaryNavi;tolino"]');
-    }
-    getHörbuchSection() {
-        return cy.get('a[data-gat="primaryNavi;Hörbuch"]');
-    }
-    getKinderbuchSection() {
-        return cy.get('a[data-gat="primaryNavi;Kinderbuch"]');
-    }
-    getJugendbuchSection() {
-        return cy.get('a[data-gat="primaryNavi;Jugendbuch"]');
-    }
-    getRatgeberSection() {
-        return cy.get('a[data-gat="primaryNavi;Ratgeber"]');
-    }
-    getSchuleSection() {
-        return cy.get('a[data-gat="primaryNavi;Schule"]');
-    }
-    getKalenderSection() {
-        return cy.get('a[data-gat="primaryNavi;Kalender"]');
-    }
-    getEnglischeBuecherSection() {
-        return cy.get('a[data-gat="primaryNavi;Englische Bücher"]');
-    }
-    getSaleSection() {
-        return cy.get('a[data-gat="primaryNavi;SALE"]');
-    }
-    getSpielzeugSection() {
-        return cy.get('a[data-gat="primaryNavi;Spielzeug"]');
-    }
-    getMusikSection() {
-        return cy.get('a[data-gat="primaryNavi;Musik"]');
-    }
-    getFilmSection() {
-        return cy.get('a[data-gat="primaryNavi;Film"]');
-    }
-    getGamesSection() {
-        return cy.get('a[data-gat="primaryNavi;Games"]');
-    }
-    getFanartikelSection() {
-        return cy.get('a[data-gat="primaryNavi;Fanartikel"]');
-    }
-    getTechnikSection() {
-        return cy.get('a[data-gat="primaryNavi;Technik"]');
-    }
-    getWohnenSection() {
-        return cy.get('a[data-gat="primaryNavi;Wohnen & Leben"]');
-    }
-    getSchreibwarenSection() {
-        return cy.get('a[data-gat="primaryNavi;Schreibwaren"]');
-    }
-    getBuchreihenSection() {
-        return cy.get('a[data-gat="primaryNavi;Buchreihen"]');
-    }
-    getMagazinSection() {
-        return cy.get('a[data-gat="primaryNavi;Magazin"]');
-    }
-    getServicePortalButton() {
-        return cy.get('#chatbot-start-icon');
-    }
+   
     getNewsletterButton() {
-        return cy.get('a[title="Jetzt kostenlosen Newsletter abonnieren"]');
-    }
-    getBannerSlider() {
-        return cy.get('ul.plain.slides.top-banners');
-    }
-    getSidebarBestsellerSection() {
-        return cy.contains('a.without-icon', 'Bestseller');
-    }
-    
-    getSidebarNeuheitenSection() {
-        return cy.contains('a.without-icon', 'Neuheiten');
-    }
-    
-    getSidebarVorschauSection() {
-        return cy.contains('a.without-icon', 'Vorschau');
-    }
-    
-    getSidebarPreishitsSection() {
-        return cy.contains('a.without-icon', 'Preishits');
-    }
-    
-    getSidebarGeschenkgutscheineSection() {
-        return cy.contains('a.without-icon', 'Geschenkgutscheine');
+    return cy.get('button[title="Zum Newsletter eintragen"]');
     }
 
-    getSidebarBuchreihenSection() {
-        return cy.contains('a', 'Buchreihen');
-    }
-
-    getSidebarKochbuchshopSection() {
-        return cy.contains('a', 'Kochbuchshop');
-    }
-      
-    getSidebarGregShopSection() {
-        return cy.contains('a', 'Greg-Shop');
-    }
-      
-    getSidebarReisefuehrerSection() {
-        return cy.contains('a', 'Reiseführer');
-    }
-      
-    getSidebarStarWarsShopSection() {
-        return cy.contains('a', 'Star Wars-Shop');
-    }
-      
-    getSidebarNintendoShopSection() {
-        return cy.contains('a', 'Nintendo-Shop');
-    }
-      
-    getSidebarStabiloShopSection() {
-        return cy.contains('a', 'STABILO-Shop');
-    }
-      
-    getSidebarBravoHitsSection() {
-        return cy.contains('a', 'Bravo Hits');
-    }
-      
-    getSidebarLegoshopSection() {
-        return cy.contains('a', 'LEGO® Shop');
-    }
-      
-    getSidebarGUShopSection() {
-        return cy.contains('a', 'GU-Shop');
-    }
-    getBestsellerBooksBlock() {
-        return cy.get('a.headline-link').contains('Bestseller - Bücher');
-    }
-
-    getBestsellerMoviesBlock() {
-        return cy.get('a.headline-link').contains('Bestseller - Filme');
-    }
-    
-    getBestsellerMusicBlock() {
-        return cy.get('a.headline-link').contains('Bestseller - Musik');
-    }
-    
-    getBestsellerToysBlock() {
-        return cy.get('a.headline-link').contains('Bestseller - Spielwaren');
-    }
-    
-    getBestsellerGamesBlock() {
-        return cy.get('a.headline-link').contains('Bestseller - Games');
-    }
-    
-    getBestsellerStationeryBlock() {
-        return cy.get('a.headline-link').contains('Bestseller - Schreibwaren');
-    }
     getNewsletterInput() {
-        return cy.get('#form_email_benefit');
+        return cy.get('input[placeholder="Deine E-Mail-Adresse"]');
     }
 
     getFooterAccountLink() {
