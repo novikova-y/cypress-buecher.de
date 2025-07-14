07683c258368b3e793c4546191c39dfc5e37133d
Yulia Novikova <ysn.novikova@gmail.com>
2025-07-14 22:50:21 +0200

    Fix Payback cart test and optimize main page tests

--- cypress/e2e/mainPage.cy.js
+++ cypress/e2e/mainPage.cy.js
@@ -2,7 +2,6 @@ import { mainPage } from "../support/pages/mainPage";
 
 describe('Buecher.de main page', () => {
   beforeEach(() => {
-    // Set cookies for consent acceptance
     cy.setConsentCookies();
     mainPage.visit();
   });
@@ -12,70 +11,67 @@ describe('Buecher.de main page', () => {
   });
 
   describe('Main page elements', () => {
-    it('should display the logo', () => {
-      mainPage.getLogo().should('be.visible');
-    });
+    const mainElements = [
+      { name: 'logo', getter: mainPage.getLogo },
+      { name: 'search input', getter: mainPage.getSearchInput },
+      { name: 'registration link', getter: mainPage.getRegistrationLink },
+      { name: 'wishlist icon', getter: mainPage.getWishlistIcon },
+      { name: 'payback icon', getter: mainPage.getPaybackIcon },
+      { name: 'shopping basket', getter: mainPage.getShoppingBasket },
+      { name: 'newsletter button', getter: mainPage.getNewsletterButton },
+      { name: 'newsletter input', getter: mainPage.getNewsletterInput }
+    ];
 
-    it('should display the search input', () => {
-      mainPage.getSearchInput().should('be.visible');
+    it('should display all main page elements', () => {
+      mainElements.forEach(({ getter, name }) => {
+        getter().should('be.visible');
+      });
     });
-
-    it('should display the registration link', () => {
-      mainPage.getRegistrationLink().should('be.visible');
-    });
-
-    it('should display the Wishlist icon', () => {
-      mainPage.getWishlistIcon().should('be.visible');
-    });
-
-    it('should display the Payback icon', () => {
-      mainPage.getPaybackIcon().should('be.visible');
-    });
-
-    it('should display the Shopping basket', () => {
-      mainPage.getShoppingBasket().should('be.visible');
-    });
-
-    it('should display the Newsletter registration button', () => {
-      mainPage.getNewsletterButton().should('be.visible');
-    });
-
-    it('should display the Newsletter registration email input', () => {
-      mainPage.getNewsletterButton().should('be.visible');
-    });
-
   });
 
   describe('Footer elements', () => {
-    it('should display Footer links', () => {
-      mainPage.getFooterOrdersLink().should('be.visible');
-      mainPage.getFooterRuecksendungLink().should('be.visible');
-      mainPage.getFooterWiderrufLink().should('be.visible');
-      mainPage.getFooterRechnungLink().should('be.visible');
-      mainPage.getFooterPaypalLink().should('be.visible');
-      mainPage.getFooterKreditkarteLink().should('be.visible');
-      mainPage.getFooterOtherPaymentMethodsLink().should('be.visible');
-      mainPage.getFooterKontaktLink().should('be.visible');
-      mainPage.getFooterLieferungLink().should('be.visible');
-      mainPage.getFooterDatenschutzLink().should('be.visible');
-      mainPage.getFooterFaqLink().should('be.visible');
-      mainPage.getFooterAgbLink().should('be.visible');
-      mainPage.getFooterImpressumLink().should('be.visible');
-      mainPage.getFooterPartnerprogrammLink().should('be.visible');
-      mainPage.getFooterPressemitteilungenLink().should('be.visible');
+    const footerLinks = [
+      mainPage.getFooterOrdersLink,
+      mainPage.getFooterRuecksendungLink,
+      mainPage.getFooterWiderrufLink,
+      mainPage.getFooterRechnungLink,
+      mainPage.getFooterPaypalLink,
+      mainPage.getFooterKreditkarteLink,
+      mainPage.getFooterOtherPaymentMethodsLink,
+      mainPage.getFooterKontaktLink,
+      mainPage.getFooterLieferungLink,
+      mainPage.getFooterDatenschutzLink,
+      mainPage.getFooterFaqLink,
+      mainPage.getFooterAgbLink,
+      mainPage.getFooterImpressumLink,
+      mainPage.getFooterPartnerprogrammLink,
+      mainPage.getFooterPressemitteilungenLink,
+    ];
+
+    it('should display all footer links', () => {
+      footerLinks.forEach(getter => {
+        getter().should('be.visible');
+      });
     });
   });
 
   describe('Social media and service icons', () => {
+    const socialIcons = [
+      mainPage.getFacebookIcon,
+      mainPage.getInstagramIcon,
+      mainPage.getYouTubeIcon,
+      mainPage.getTikTokIcon,
+      mainPage.getRechnungIcon,
+      mainPage.getPayPalIcon,
+      mainPage.getKreditkarteIcon,
+    ];
+
     it('should display social media and service icons', () => {
-      mainPage.getFacebookIcon().should('be.visible');
-      mainPage.getInstagramIcon().should('be.visible');
-      mainPage.getYouTubeIcon().should('be.visible');
-      mainPage.getTikTokIcon().should('be.visible');
-      mainPage.getRechnungIcon().should('be.visible');
-      mainPage.getPayPalIcon().should('be.visible');
-      mainPage.getKreditkarteIcon().should('be.visible');
-      mainPage.getPaybackServiceIcon().should('be.visible');
+      socialIcons.forEach(getter => {
+        getter().should('be.visible');
+      });
+      // Scroll only for the one element that requires it
+      mainPage.getPaybackServiceIcon().scrollIntoView().should('be.visible');
     });
   });
 });
--- cypress/support/pages/mainPage.js
+++ cypress/support/pages/mainPage.js
@@ -1,126 +1,144 @@
 export class MainPage {
-
-    visit() {
-      cy.visit('/');
-    }
-  
-    getTitle() {
-        return cy.title();
-    }
-    getLogo() {
-        return cy.get('img[alt="bücher.de"]').should('be.visible');
-    }
-    getSearchInput() {
-        return cy.get('#qusearchfield');
-    }
-    getRegistrationLink() {
-        return cy.get('a[alt="Mein Kundenkonto"]');
-    }
-    getWishlistIcon() {
-        return cy.get('a[data-tooltip="Meine Merkliste"]');
-    }
-    getPaybackIcon() {
-        return cy.get('img[alt^="PAYBACK Logo"]');
-    }
-    getShoppingBasket() {
-        return cy.get('a[data-behavior="cartCount"]').contains('Warenkorb');
-    }
-   
-    getNewsletterButton() {
-        return cy.get('button[title="Zum Newsletter eintragen"]');
-    }
-
-    getNewsletterInput() {
-        return cy.get('input[placeholder="Deine E-Mail-Adresse"]');
-    }
-
-    getFooterOrdersLink() {
-        return cy.contains('a[href*="/my_orders"]', 'Meine Bestellungen');
-    }
-    
-    getFooterGutscheinLink() {
-        return cy.contains('a[href*="/answers/id/05/"]', 'Gutschein einlösen');
-    }
-    
-    getFooterRuecksendungLink() {
-        return cy.contains('a', 'Rücksendung');
-    }
-
-    getFooterWiderrufLink() {
-        return cy.contains('a','Widerruf');
-    }
-
-    getFooterRechnungLink() {
-        return cy.contains('a','Rechnung');
-    }
-
-    getFooterPaypalLink() {
-        return cy.contains('a','PayPal');
-    }
-
-    getFooterKreditkarteLink() {
-        return cy.contains('a','Kreditkarte');
-    }
-
-    getFooterOtherPaymentMethodsLink() {
-        return cy.contains('a','Weitere Zahlungsarten');
-    }
-    getFooterKontaktLink() {
-        return cy.contains('a','Kontakt');
-    }
-    getFooterLieferungLink() {
-        return cy.contains('a','Lieferung & Versand');
-    }
-    getFooterDatenschutzLink() {
-        return cy.contains('a','Datenschutz');
-    }
-    
-    getFooterFaqLink() {
-        return cy.contains('a','FAQ');
-    }
-    getFooterImpressumLink() {
-        return cy.contains('a','Impressum');
-    }
-    getFooterAgbLink() {
-        return cy.contains('a','AGB');
-    }
-    
-    getFooterPartnerprogrammLink() {
-        return cy.contains('a','Partnerprogramm');
-    }
-    
-    getFooterPressemitteilungenLink() {
-        return cy.contains('a','Pressemitteilungen');
-    }
-
-    getInstagramIcon() {
-        return cy.get('a[data-tooltip="Folge uns auf Instagram"]');
-    }
-
-    getFacebookIcon() {
-        return cy.get('a[data-tooltip="Folge uns auf Facebook"]');
-    }
-    
-    getYouTubeIcon() {
-        return cy.get('a[data-tooltip="Folge uns auf YouTube"]');
-    }
-    getTikTokIcon() {
-        return cy.get('a[data-tooltip="Folge uns auf TikTok"]');
-    }
-    getRechnungIcon() {
-        return cy.get('a[data-tooltip="Bezahlung auf Rechnung"]');
-    }
-    getPayPalIcon() {
-        return cy.get('a[data-tooltip="Bezahlung mit PayPal"]');
-    }
-    getKreditkarteIcon() {
-        return cy.get('a[data-tooltip="Bezahlung mit Kreditkarte"]');
-    }
-    getPaybackServiceIcon() {
-        return cy.get('img[alt="Payback-Logo mit lachendem Pointee"]');
-    }
-
+  // Navigation
+  visit() {
+    cy.visit('/');
   }
-  
-  export const mainPage = new MainPage();
-  
+
+  // Page title
+  getTitle() {
+    return cy.title();
+  }
+
+  // Header elements
+  getLogo() {
+    return cy.get('img[alt="bücher.de"]');
+  }
+
+  getSearchInput() {
+    return cy.get('#qusearchfield');
+  }
+
+  getRegistrationLink() {
+    return cy.get('a[alt="Mein Kundenkonto"]');
+  }
+
+  getWishlistIcon() {
+    return cy.get('a[data-tooltip="Meine Merkliste"]');
+  }
+
+  getPaybackIcon() {
+    return cy.get('img[alt^="PAYBACK Logo"]');
+  }
+
+  getShoppingBasket() {
+    return cy.get('a[data-behavior="cartCount"]').contains('Warenkorb');
+  }
+
+  getNewsletterButton() {
+    return cy.get('button[title="Zum Newsletter eintragen"]');
+  }
+
+  getNewsletterInput() {
+    return cy.get('input[placeholder="Deine E-Mail-Adresse"]');
+  }
+
+  // Footer links
+  getFooterOrdersLink() {
+    return cy.contains('a[href*="/my_orders"]', 'Meine Bestellungen');
+  }
+
+  getFooterGutscheinLink() {
+    return cy.contains('a[href*="/answers/id/05/"]', 'Gutschein einlösen');
+  }
+
+  getFooterRuecksendungLink() {
+    return cy.contains('a', 'Rücksendung');
+  }
+
+  getFooterWiderrufLink() {
+    return cy.contains('a', 'Widerruf');
+  }
+
+  getFooterRechnungLink() {
+    return cy.contains('a', 'Rechnung');
+  }
+
+  getFooterPaypalLink() {
+    return cy.contains('a', 'PayPal');
+  }
+
+  getFooterKreditkarteLink() {
+    return cy.contains('a', 'Kreditkarte');
+  }
+
+  getFooterOtherPaymentMethodsLink() {
+    return cy.contains('a', 'Weitere Zahlungsarten');
+  }
+
+  getFooterKontaktLink() {
+    return cy.contains('a', 'Kontakt');
+  }
+
+  getFooterLieferungLink() {
+    return cy.contains('a', 'Lieferung & Versand');
+  }
+
+  getFooterDatenschutzLink() {
+    return cy.contains('a', 'Datenschutz');
+  }
+
+  getFooterFaqLink() {
+    return cy.contains('a', 'FAQ');
+  }
+
+  getFooterImpressumLink() {
+    return cy.contains('a', 'Impressum');
+  }
+
+  getFooterAgbLink() {
+    return cy.contains('a', 'AGB');
+  }
+
+  getFooterPartnerprogrammLink() {
+    return cy.contains('a', 'Partnerprogramm');
+  }
+
+  getFooterPressemitteilungenLink() {
+    return cy.contains('a', 'Pressemitteilungen');
+  }
+
+  // Social media and payment icons
+  getInstagramIcon() {
+    return cy.get('a[data-tooltip="Folge uns auf Instagram"]');
+  }
+
+  getFacebookIcon() {
+    return cy.get('a[data-tooltip="Folge uns auf Facebook"]');
+  }
+
+  getYouTubeIcon() {
+    return cy.get('a[data-tooltip="Folge uns auf YouTube"]');
+  }
+
+  getTikTokIcon() {
+    return cy.get('a[data-tooltip="Folge uns auf TikTok"]');
+  }
+
+  getRechnungIcon() {
+    return cy.get('a[data-tooltip="Bezahlung auf Rechnung"]');
+  }
+
+  getPayPalIcon() {
+    return cy.get('a[data-tooltip="Bezahlung mit PayPal"]');
+  }
+
+  getKreditkarteIcon() {
+    return cy.get('a[data-tooltip="Bezahlung mit Kreditkarte"]');
+  }
+
+  getPaybackServiceIcon() {
+    return cy.get('img[alt="Payback-Logo mit lachendem Pointee"]');
+  }
+}
+
+export const mainPage = new MainPage();
